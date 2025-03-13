2ded15477159476e0d9088a1b0a4a239d931f7f5
josephwilms7 <josephrwilms@gmail.com>
2025-03-13 10:07:50 -0600

    Week 1 and 2

---
+++ database/test.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+VALUES   (
+    'Test',
+    'User',
+	'test@gmail.com',
+	'password'
+);
