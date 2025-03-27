c94082941daeaa9aca56480d660bb9bc06fbcd95
josephwilms7 <josephrwilms@gmail.com>
2025-03-27 08:23:18 -0600

    Server Side Validation not working?

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,11 +2,15 @@ const express = require("express")
 const router = new express.Router() 
 const accController = require("../controllers/accController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 router.get("/", accController.buildAccount)
 router.get("/login", accController.buildLogin)
 router.get("/register", accController.buildRegister)
 
-router.post('/register', utilities.handleErrors(accController.registerAccount))
+router.post('/register',
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accController.registerAccount))
 
 module.exports = router;
